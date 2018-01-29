929364081cb81725b18c940c50f09e3637705b79
Nikita Trubkin <trubkin.nikita@yandex.ru>
2018-01-29 18:49:44 +0300

    Написаны основные скрипты базы данных

---
+++ src/main/resources/SQL/create.sql
@@ -0,0 +1,49 @@
+CREATE TABLE groups
+(
+  id        SERIAL      NOT NULL
+    CONSTRAINT groups_pkey
+    PRIMARY KEY,
+  name      VARCHAR(40) NOT NULL,
+  leader_id INTEGER     NOT NULL
+);
+
+CREATE TABLE accounts
+(
+  id       SERIAL      NOT NULL
+    CONSTRAINT accounts_pkey
+    PRIMARY KEY,
+  name     VARCHAR(40) NOT NULL,
+  group_id INTEGER
+    CONSTRAINT accounts_groups_id_fk
+    REFERENCES groups
+    ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+ALTER TABLE public.groups
+  ADD CONSTRAINT groups_accounts_id_fk
+FOREIGN KEY (leader_id) REFERENCES accounts (id) ON UPDATE CASCADE;
+
+CREATE TABLE public.lessons
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(40) NOT NULL,
+  room INT,
+  dateTime TIMESTAMP NOT NULL,
+  teacher VARCHAR(40),
+  group_id INT NOT NULL,
+  CONSTRAINT lessons_groups_id_fk FOREIGN KEY (group_id) REFERENCES groups (id) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE public.semester_starts
+(
+  group_id INT NOT NULL,
+  date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE public.events
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(40) NOT NULL,
+  dateTime TIMESTAMP NOT NULL,
+  description VARCHAR(1000) NOT NULL
+);
---
+++ src/main/resources/SQL/drop.sql
@@ -0,0 +1,7 @@
+ALTER TABLE public.groups DROP CONSTRAINT groups_accounts_id_fk;
+
+DROP TABLE public.events;
+DROP TABLE public.semester_starts;
+DROP TABLE public.lessons;
+DROP TABLE public.accounts;
+DROP TABLE public.groups;
